24d2c8441022f25fd529ccc6dee886ab767ecdc7
FrankCW <wongasa999@gmail.com>
2024-10-06 13:25:50 +0700

    Edit_q8

--- question-8.js
+++ question-8.js
@@ -2,13 +2,9 @@
 
 async function fetcUsers() {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
         const users = await response.json();
-
         const newusers = users.map(user => user.name);
         console.log(newusers);
     } catch (error) {
